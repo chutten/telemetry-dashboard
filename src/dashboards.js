b82733e2a6e8e329e5edbd99a833fbab95606c08
Hemant Singh <hemantsingh1612@gmail.com>
2017-04-11 04:44:56 +0530

    updated dashboards.js
    added document.execCommand('copy')

--- src/dashboards.js
+++ src/dashboards.js
@@ -163,7 +163,8 @@ $(document)
               .find("input")
               .show()
               .val(shortUrl)
-              .focus();
+              .focus()
+              .document.execCommand('copy');
           }
         });
       });
